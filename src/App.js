faae996132eb6d42d8c52b1972c7d3aebbf86e2c
Natalia Barrionuevo <barrionuevonatalia1998@gmail.com>
2023-07-24 19:53:16 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Natalia Barrionuevo and is
-        <a
-          href="https://github.com/NaliNat/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-        on Github
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Natalia Barrionuevo and is{" "}
+          <a
+            href="https://github.com/NaliNat/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on Github
+        </footer>
+      </div>
     </div>
   );
 }
