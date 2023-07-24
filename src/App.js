a3dff6d8afddabb04e561315d6cfc5c279a4082b
Natalia Barrionuevo <barrionuevonatalia1998@gmail.com>
2023-07-24 19:42:12 -0300

    corrected minor errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Natalia Barrionuevo and is
+        <a
+          href="https://github.com/NaliNat/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
