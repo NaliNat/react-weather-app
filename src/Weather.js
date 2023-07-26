fb91c8ba08742291efbf6d1f019d2ee1d9d7ebfc
Natalia Barrionuevo <barrionuevonatalia1998@gmail.com>
2023-07-26 20:32:38 -0300

    Added icon url

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -16,14 +18,29 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       temp_min: response.data.main.temp_min,
       temp_max: response.data.main.temp_max,
-      icon: response.data.weather.icon,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
+  }
+
+  function search() {
+    const apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -31,6 +48,7 @@ export default function Weather(props) {
                 placeholder="Enter a city..."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -42,43 +60,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-
-        <div className="row mt-3">
-          <div className="col-6">
-            <img
-              src={weatherData.icon}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-              <li>
-                {Math.round(weatherData.temp_max)}°{" "}
-                {Math.round(weatherData.temp_min)}°
-              </li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo info={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.info.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.info.date} />
+        </li>
+        <li className="text-capitalize">{props.info.description}</li>
+      </ul>
+
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src={props.info.icon}
+            alt={props.info.description}
+            className="float-left"
+          />
+          <span className="temperature">
+            {Math.round(props.info.temperature)}
+          </span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.info.humidity}%</li>
+            <li>Wind: {props.info.wind} km/h</li>
+            <li>
+              {Math.round(props.info.temp_max)}°{" "}
+              {Math.round(props.info.temp_min)}°
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
