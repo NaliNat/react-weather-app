7af163325939e85cb29fb47b188020f5d8de607b
Natalia Barrionuevo <barrionuevonatalia1998@gmail.com>
2023-08-04 13:26:28 -0300

    added weather forecast for one day

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,28 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="WeatherForecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-max-temp">
+          {Math.round(props.data.temp.max)}°
+        </span>
+        <span className="WeatherForecast-min-temp">
+          {Math.round(props.data.temp.min)}°
+        </span>
+      </div>
+    </div>
+  );
+}
